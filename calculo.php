f08cf7db7d84ff55757267240bab2eab6e57b74a
Nyna <45694364+Nyna-byte@users.noreply.github.com>
2022-05-05 23:05:47 +0200

    Add files via upload

--- calculo.php
+++ calculo.php
@@ -1,16 +1,16 @@
 <?php
 
-$cantidad=$_POST["cantidad"];
-$arrTC=$_POST["tc"];
-$tiempo=$_POST["tiempo"]; 
+$cantidad=$_POST['cantidad'];
+$arrTC=$_POST['TC'];
+$tiempo=$_POST['tiempo']; 
 
 $TCtotal=0;
-for($i=0; $i<$arrTC.length; $i++){
+for($i=0; $i<sizeof($arrTC); $i++){
     $TCtotal+=$arrTC[$i];
 }
 if($TCtotal!==0){
-    $operarios=($cantidad*($TCtotal÷100))/$tiempo; //El tc entre 100 porque son centésimas de minuto
-    $equilibrado=(($TCtotal÷100)/$operarios)*100;
+    $operarios=($cantidad*($TCtotal/100))/$tiempo; //El tc entre 100 porque son centésimas de minuto
+    $equilibrado=(($TCtotal/100)/$operarios)*100;
     echo "Número de operiarios: $operarios";
     echo "Base de equilibrado: $equilibrado";
 }
